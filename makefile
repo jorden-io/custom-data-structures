418b5dcd84c3920ce7876eb62d2e75f19aa41e3b
JorbyWasTaken <jordenrodriguez1004@yahoo.com>
2022-12-23 15:46:31 -0600

    init

---
+++ makefile
@@ -0,0 +1,3 @@
+defualt:
+	clang++ -Wall -std=c++17 src/*.cpp -o brainFuck
+	./brainFuck nums.txt
---
+++ src/linked.hpp
@@ -0,0 +1,5 @@
+#include <string>
+#define LINKED_HPP
+#ifdef LINKED_HPP
+extern void show_l(std::string s);
+#endif
